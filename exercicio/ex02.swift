2e6a5b09ec0b2c0ea66b2ad3fae565944134dc9a
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 20:39:26 -0300

    Verificação de Par ou Ímpar

---
+++ exercicio/ex02.swift
@@ -0,0 +1,10 @@
+// Escreva um código que verifique se um número dado é par ou ímpar.
+// Objetivo: Praticar a estrutura de condicionais (if/else).
+
+let numero = 3
+
+if numero % 2 == 0 {
+    print("O número \(numero) é Par")
+} else {
+    print("O número \(numero) é Ímpar")
+}
