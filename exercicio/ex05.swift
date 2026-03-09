1b548eb12f32349ad614fa8370c99999fbcff3eb
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-08 23:04:48 -0300

    Iterando sobre um Array de Nomes

---
+++ exercicio/ex05.swift
@@ -0,0 +1,8 @@
+// Crie um array contendo 5 nomes e use um loop para imprimir todos os nomes na tela.
+// Objetivo: Praticar o uso de arrays e loops
+
+let nomes = ["Alice", "Bob", "Carlos", "Diana", "Eduarda"]
+
+for nome in nomes{
+    print(nome)
+}
