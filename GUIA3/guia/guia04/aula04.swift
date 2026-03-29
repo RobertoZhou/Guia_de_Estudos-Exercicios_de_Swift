f099b8d8ebfcb0ea98f286f2be1196d129c20ee7
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-03-28 22:44:25 -0300

    Associated Types em Protocolos

--- GUIA3/guia/guia04/aula04.swift
+++ GUIA3/guia/guia04/aula04.swift
@@ -1,2 +1,26 @@
 // Associated Types em Protocolos
 
+protocol Container {
+    associatedtype Item
+
+    mutating func append(_ item: Item)
+    var count: Int { get }
+    subscript(i: Int) -> Item { get }
+}
+
+struct ArrayInt: Container {
+    typealias Item = Int
+    var itens: [Int] = []
+
+    mutating func append(_ item: Item) {
+        itens.append(item)
+    }
+
+    var count: Int {
+        return itens.count
+    }
+
+    subscript(i: Int) -> Item {
+        return itens[i]
+    }
+}
